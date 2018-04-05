1c2a18912943f483b9bba4501f221599fea28736
DeqingQu <qu.deqing@gmail.com>
2018-04-04 18:07:46 -0700

    mkdir

--- neo4j-back.sh
+++ neo4j-back.sh
@@ -10,6 +10,9 @@ echo 'shut down Neo4j ...'
 service neo4j stop
 
 echo 'start backup ...'
+if [ ! -d "/mnt/data/test/"]; then
+    mkdir /mnt/data/test/
+fi
 neo4j-admin dump --database=graph --to=/mnt/data/test/$file.cypher
 
 echo 'backup complete ...'
