7473fbe40c8bc09999822b40d71c324e262dcb6b
DeqingQu <qu.deqing@gmail.com>
2018-04-11 17:27:34 -0700

    modify the unittest configuration

--- Neo4jConnectionTests.py
+++ Neo4jConnectionTests.py
@@ -9,6 +9,7 @@ import unittest
 from Neo4jConnection import Neo4jConnection
 import json
 
+
 class Neo4jConnectionTestCase(unittest.TestCase):
 
     def test_get_protein_nodes(self):
@@ -19,14 +20,15 @@ class Neo4jConnectionTestCase(unittest.TestCase):
         user = json.loads(user_data)
 
         conn = Neo4jConnection("bolt://localhost:7687", user['username'], user['password'])
+        nodes = conn.get_protein_node("P53814")
 
-        nodes = conn.get_protein_nodes()
         self.assertIsNotNone(nodes)
-        self.assertEqual(len(nodes), 5)
+        self.assertEqual(nodes['n']['name'], "P53814")
+        self.assertEqual(nodes['n']['curie_id'], "UniProt:P53814")
 
         conn.close()
 
-    def test_get_protein_nodes(self):
+    def test_get_protein_node(self):
 
         f = open('user_pass.json', 'r')
         user_data = f.read()
@@ -34,11 +36,11 @@ class Neo4jConnectionTestCase(unittest.TestCase):
         user = json.loads(user_data)
 
         conn = Neo4jConnection("bolt://localhost:7687", user['username'], user['password'])
+        nodes = conn.get_protein_nodes()
 
-        nodes = conn.get_protein_node("P53814")
         self.assertIsNotNone(nodes)
-        self.assertEqual(nodes['n']['name'], "P53814")
-        self.assertEqual(nodes['n']['curie_id'], "UniProt:P53814")
+        self.assertEqual(len(nodes), 5)
+        self.assertEqual(nodes[0], "P53814")
 
         conn.close()
 
