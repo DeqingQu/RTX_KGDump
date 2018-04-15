1a4947401714ce7c03d08200e30f40b8c124ab0b
DeqingQu <qu.deqing@gmail.com>
2018-04-15 13:24:23 -0700

    Query from MyChem.info, fix a potential bug about double quote and quote

---
+++ QueryMyChem.py
@@ -0,0 +1,70 @@
+''' This module defines the class QueryMyChem. QueryMyChem class is designed
+to communicate with MyChem APIs and their corresponding data sources. The available methods include:
+
+    get_chemical_substance_entity : query chemical substance properties by ID
+
+'''
+
+__author__ = 'Deqing Qu'
+__copyright__ = 'Oregon State University'
+__credits__ = ['Deqing Qu', 'Stephen Ramsey']
+__license__ = 'MIT'
+__version__ = '0.1.0'
+__maintainer__ = ''
+__email__ = ''
+__status__ = 'Prototype'
+
+import requests
+import requests_cache
+import sys
+import json
+
+# configure requests package to use the "orangeboard.sqlite" cache
+requests_cache.install_cache('orangeboard')
+
+
+class QueryMyChem:
+    TIMEOUT_SEC = 120
+    API_BASE_URL = 'http://mychem.info/v1'
+    HANDLER_MAP = {
+        'get_chemical_substance': 'chem/{id}',
+    }
+
+    @staticmethod
+    def __access_api(handler):
+
+        url = QueryMyChem.API_BASE_URL + '/' + handler
+
+        try:
+            res = requests.get(url, timeout=QueryMyChem.TIMEOUT_SEC)
+        except requests.exceptions.Timeout:
+            print(url, file=sys.stderr)
+            print('Timeout in QueryBioLink for URL: ' + url, file=sys.stderr)
+            return None
+        status_code = res.status_code
+        if status_code != 200:
+            print(url, file=sys.stderr)
+            print('Status code ' + str(status_code) + ' for url: ' + url, file=sys.stderr)
+            return None
+
+        return res.text
+
+    @staticmethod
+    def __get_entity(entity_type, entity_id):
+        handler = QueryMyChem.HANDLER_MAP[entity_type].format(id=entity_id)
+        results = QueryMyChem.__access_api(handler)
+        result_str = 'UNKNOWN'
+        if results is not None:
+            json_dict = json.loads(results)
+            result_str = json.dumps(json_dict)
+
+        return result_str
+
+    @staticmethod
+    def get_chemical_substance_entity(chemical_substance_id):
+        return QueryMyChem.__get_entity("get_chemical_substance", chemical_substance_id)
+
+
+if __name__ == '__main__':
+    result = QueryMyChem.get_chemical_substance_entity('CHEMBL1201217')
+    print(result)
