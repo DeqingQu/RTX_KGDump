58822d30009383db2b3731f633660e1cee7661a5
DeqingQu <qu.deqing@gmail.com>
2018-04-25 21:18:09 -0700

    update desc for mygene nodes and test cases

--- Neo4jConnection.py
+++ Neo4jConnection.py
@@ -135,6 +135,14 @@ class Neo4jConnection:
         with self._driver.session() as session:
             return session.write_transaction(self._update_phenotype_nodes_desc, nodes)
 
+    def update_microRNA_nodes_desc(self, nodes):
+        with self._driver.session() as session:
+            return session.write_transaction(self._update_microRNA_nodes_desc, nodes)
+
+    def update_protein_nodes_desc(self, nodes):
+        with self._driver.session() as session:
+            return session.write_transaction(self._update_protein_nodes_desc, nodes)
+
     def update_disease_nodes_desc(self, nodes):
         with self._driver.session() as session:
             return session.write_transaction(self._update_disease_nodes_desc, nodes)
@@ -354,12 +362,38 @@ class Neo4jConnection:
         return result
 
     @staticmethod
+    def _update_microRNA_nodes_desc(tx, nodes):
+        result = tx.run(
+            """
+            UNWIND {nodes} AS row
+            WITH row.node_id AS node_id, row.desc AS description
+            MATCH (n:microRNA{id:node_id})
+            SET n.description=description
+            """,
+            nodes=nodes,
+        )
+        return result
+
+    @staticmethod
     def _update_disease_nodes_desc(tx, nodes):
         result = tx.run(
             """
             UNWIND {nodes} AS row
             WITH row.node_id AS node_id, row.desc AS description
             MATCH (n:disease{id:node_id})
+            SET n.description=description
+            """,
+            nodes=nodes,
+        )
+        return result
+
+    @staticmethod
+    def _update_protein_nodes_desc(tx, nodes):
+        result = tx.run(
+            """
+            UNWIND {nodes} AS row
+            WITH row.node_id AS node_id, row.desc AS description
+            MATCH (n:protein{id:node_id})
             SET n.description=description
             """,
             nodes=nodes,
