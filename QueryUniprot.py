49fc781d131f31eb94b1432d3ddc8bcc903072a7
DeqingQu <qu.deqing@gmail.com>
2018-05-09 18:29:05 -0700

    map ec to uniprot ids

---
+++ QueryUniprot.py
@@ -0,0 +1,92 @@
+""" This module defines the class QueryPharos which connects to APIs at
+http://www.uniprot.org/uploadlists/, querying reactome pathways from uniprot id.
+"""
+
+__author__ = ""
+__copyright__ = ""
+__credits__ = []
+__license__ = ""
+__version__ = ""
+__maintainer__ = ""
+__email__ = ""
+__status__ = "Prototype"
+
+import requests
+import CachedMethods
+import sys
+import urllib.parse
+
+
+class QueryUniprot:
+    API_BASE_URL = "http://www.uniprot.org/uploadlists/"
+    TIMEOUT_SEC = 120
+    HANDLER_MAP = {
+        'map_enzyme_commission_id_to_uniprot_ids': 'uniprot/?query=({id})&format=tab&columns=id'
+    }
+
+    @staticmethod
+    @CachedMethods.register
+    def uniprot_id_to_reactome_pathways(uniprot_id):
+        """returns a ``set`` of reactome IDs of pathways associated with a given string uniprot ID
+
+        :param uniprot_id: a ``str`` uniprot ID, like ``"P68871"``
+        :returns: a ``set`` of string Reactome IDs
+        """
+
+        payload = { 'from':   'ACC',
+                    'to':     'REACTOME_ID',
+                    'format': 'tab',
+                    'query':  uniprot_id }
+        contact = "stephen.ramsey@oregonstate.edu"
+        header = {'User-Agent': 'Python %s' % contact}
+        res = requests.post(QueryUniprot.API_BASE_URL, data=payload, headers=header)
+        assert 200 == res.status_code
+        res_set = set()
+        for line in res.text.splitlines():
+            field_str = line.split("\t")[1]
+            if field_str != "To":
+                res_set.add(field_str)
+        return res_set
+
+    @staticmethod
+    def __access_api(handler):
+
+        api_base_url = 'http://www.uniprot.org'
+        url = api_base_url + '/' + handler
+        print(url)
+        contact = "stephen.ramsey@oregonstate.edu"
+        header = {'User-Agent': 'Python %s' % contact}
+        try:
+            res = requests.get(url, timeout=QueryUniprot.TIMEOUT_SEC, headers=header)
+        except requests.exceptions.Timeout:
+            print(url, file=sys.stderr)
+            print('Timeout in QueryUniprot for URL: ' + url, file=sys.stderr)
+            return None
+        status_code = res.status_code
+        if status_code != 200:
+            print(url, file=sys.stderr)
+            print('Status code ' + str(status_code) + ' for url: ' + url, file=sys.stderr)
+            return None
+        return res.text
+
+    @staticmethod
+    @CachedMethods.register
+    def map_enzyme_commission_id_to_uniprot_ids(ec_id):
+        ec_id_encoded = urllib.parse.quote_plus(ec_id)
+        handler = QueryUniprot.HANDLER_MAP['map_enzyme_commission_id_to_uniprot_ids'].format(id=ec_id_encoded)
+        res = QueryUniprot.__access_api(handler)
+        arr = []
+        if res is not None:
+            res = res[res.find('\n')+1:]
+            for line in res.splitlines():
+                arr.append(line)
+        return arr
+
+
+if __name__ == '__main__':
+    # print(QueryUniprot.uniprot_id_to_reactome_pathways("P68871"))
+    # print(QueryUniprot.uniprot_id_to_reactome_pathways("Q16621"))
+    # print(QueryUniprot.uniprot_id_to_reactome_pathways("P09601"))
+    # print(CachedMethods.cache_info())
+
+    print(QueryUniprot.map_enzyme_commission_id_to_uniprot_ids("ec:4.4.1.15"))
